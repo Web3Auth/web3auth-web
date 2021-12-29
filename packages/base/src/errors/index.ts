96f33575f6131c2a6801a85352589dcc0d7e8432
himanshuchawla009 <himanshuchawla2014@gmail.com>
2021-12-29 16:15:01 +0530

    error msgs improved

--- packages/base/src/errors/index.ts
+++ packages/base/src/errors/index.ts
@@ -13,6 +13,8 @@ export type ErrorCodes = {
 };
 
 export abstract class Web3AuthError extends CustomError implements IWeb3AuthError {
+  name: string;
+
   code: number;
 
   message: string;
@@ -24,7 +26,7 @@ export abstract class Web3AuthError extends CustomError implements IWeb3AuthErro
     this.code = code;
     this.message = message || "";
     // Set name explicitly as minification can mangle class names
-    Object.defineProperty(this, "name", { value: "TkeyError" });
+    Object.defineProperty(this, "name", { value: "Web3AuthError" });
   }
 
   toJSON(): IWeb3AuthError {
@@ -40,14 +42,9 @@ export abstract class Web3AuthError extends CustomError implements IWeb3AuthErro
   }
 }
 
-/**
- * 20XX - wallet login errors
- */
-
-export class WalletLoginError extends Web3AuthError {
+export class WalletInitializationError extends Web3AuthError {
   protected static messages: ErrorCodes = {
     5000: "Custom",
-    // Misc
     5001: "Wallet is not found",
     5002: "Wallet is not installed",
     5003: "Wallet is not ready yet",
@@ -62,6 +59,63 @@ export class WalletLoginError extends Web3AuthError {
     super(code, message);
 
     // Set name explicitly as minification can mangle class names
+    Object.defineProperty(this, "name", { value: "WalletInitializationError" });
+  }
+
+  public static fromCode(code: number, extraMessage = ""): IWeb3AuthError {
+    return new WalletInitializationError(code, `${WalletInitializationError.messages[code]}${extraMessage}`);
+  }
+
+  // Custom methods
+  public static notFound(extraMessage = ""): IWeb3AuthError {
+    return WalletInitializationError.fromCode(5001, extraMessage);
+  }
+
+  public static notInstalled(extraMessage = ""): IWeb3AuthError {
+    return WalletInitializationError.fromCode(5002, extraMessage);
+  }
+
+  public static notReady(extraMessage = ""): IWeb3AuthError {
+    return WalletInitializationError.fromCode(5003, extraMessage);
+  }
+
+  public static windowBlocked(extraMessage = ""): IWeb3AuthError {
+    return WalletInitializationError.fromCode(5004, extraMessage);
+  }
+
+  public static windowClosed(extraMessage = ""): IWeb3AuthError {
+    return WalletInitializationError.fromCode(5005, extraMessage);
+  }
+
+  public static incompatibleChainNameSpace(extraMessage = ""): IWeb3AuthError {
+    return WalletInitializationError.fromCode(5006, extraMessage);
+  }
+
+  public static duplicateAdapterError(extraMessage = ""): IWeb3AuthError {
+    return WalletInitializationError.fromCode(5007, extraMessage);
+  }
+}
+
+/**
+ * wallet login errors
+ */
+
+export class WalletLoginError extends Web3AuthError {
+  protected static messages: ErrorCodes = {
+    5000: "Custom",
+    5008: "Invalid provider Config",
+    5009: "Failed to connect with wallet",
+    5010: "Failed to disconnect from wallet",
+    5011: "Wallet is not connected",
+    5012: "Failed to connect with rpc url",
+    5013: "Provider is not ready yet",
+  };
+
+  public constructor(code: number, message?: string) {
+    // takes care of stack and proto
+    super(code, message);
+
+    // Set name explicitly as minification can mangle class names
     Object.defineProperty(this, "name", { value: "WalletLoginError" });
   }
 
@@ -69,161 +123,27 @@ export class WalletLoginError extends Web3AuthError {
     return new WalletLoginError(code, `${WalletLoginError.messages[code]}${extraMessage}`);
   }
 
-  // Custom methods
-  public static notFound(extraMessage = ""): IWeb3AuthError {
-    return WalletLoginError.fromCode(5001, extraMessage);
+  public static invalidProviderConfigError(extraMessage = ""): IWeb3AuthError {
+    return WalletLoginError.fromCode(5008, extraMessage);
   }
 
-  public static notInstalled(extraMessage = ""): IWeb3AuthError {
-    return WalletLoginError.fromCode(5002, extraMessage);
+  public static connectionError(extraMessage = ""): IWeb3AuthError {
+    return WalletLoginError.fromCode(5009, extraMessage);
   }
 
-  public static notReady(extraMessage = ""): IWeb3AuthError {
-    return WalletLoginError.fromCode(5003, extraMessage);
+  public static disconnectionError(extraMessage = ""): IWeb3AuthError {
+    return WalletLoginError.fromCode(5010, extraMessage);
   }
 
-  public static windowBlocked(extraMessage = ""): IWeb3AuthError {
-    return WalletLoginError.fromCode(5004, extraMessage);
+  public static notConnectedError(extraMessage = ""): IWeb3AuthError {
+    return WalletLoginError.fromCode(5011, extraMessage);
   }
 
-  public static windowClosed(extraMessage = ""): IWeb3AuthError {
-    return WalletLoginError.fromCode(5005, extraMessage);
+  public static rpcConnectionError(extraMessage = ""): IWeb3AuthError {
+    return WalletLoginError.fromCode(5012, extraMessage);
   }
 
-  public static incompatibleChainNameSpace(extraMessage = ""): IWeb3AuthError {
-    return WalletLoginError.fromCode(5006, extraMessage);
-  }
-
-  public static duplicateAdapterError(extraMessage = ""): IWeb3AuthError {
-    return WalletLoginError.fromCode(5007, extraMessage);
-  }
-}
-
-/**
- * 20XX - wallet account/keys errors
- */
-export class WalletAccountError extends Web3AuthError {
-  name = "WalletAccountError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 2101;
-  }
-}
-
-export class WalletPublicKeyError extends Web3AuthError {
-  name = "WalletPublicKeyError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 2102;
-  }
-}
-
-export class WalletKeypairError extends Web3AuthError {
-  name = "WalletKeypairError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 2103;
-  }
-}
-
-/**
- * 10XX - wallet adapter errors (can be displayed in UI)
- */
-export class WalletConnectionError extends Web3AuthError {
-  name = "WalletConnectionError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 1001;
-  }
-}
-
-export class WalletDisconnectedError extends Web3AuthError {
-  name = "WalletDisconnectedError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 1002;
-  }
-}
-
-export class WalletDisconnectionError extends Web3AuthError {
-  name = "WalletDisconnectionError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 1003;
-  }
-}
-
-export class WalletNotConnectedError extends Web3AuthError {
-  name = "WalletNotConnectedError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 1004;
-  }
-}
-
-/**
- * 11xx - Wallet transaction/signing/blockchain errors (can be displayed in UI)
- */
-export class WalletSendTransactionError extends Web3AuthError {
-  name = "WalletSendTransactionError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 1101;
-  }
-}
-
-export class WalletSignMessageError extends Web3AuthError {
-  name = "WalletSignMessageError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 1102;
-  }
-}
-
-export class WalletSignTransactionError extends Web3AuthError {
-  name = "WalletSignTransactionError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 1103;
-  }
-}
-
-/**
- * 22xx - Wallet provider errors
- */
-export class RpcConnectionFailedError extends Web3AuthError {
-  name = "RpcConnectionFailedError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 2201;
-  }
-}
-
-export class InvalidProviderConfigError extends Web3AuthError {
-  name = "InvalidProviderConfigError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 2202;
-  }
-}
-
-export class ProviderNotReadyError extends Web3AuthError {
-  name = "ProviderNotReadyError";
-
-  constructor(message?: string, error?: Error) {
-    super(message, error);
-    this.code = 2203;
+  public static providerNotReadyError(extraMessage = ""): IWeb3AuthError {
+    return WalletLoginError.fromCode(5013, extraMessage);
   }
 }
