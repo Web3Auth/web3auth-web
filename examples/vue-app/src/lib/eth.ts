2642ca78047c025d62d15a539dce1ce52abfab94
himanshuchawla009 <himanshuchawla2014@gmail.com>
2022-01-06 00:59:36 +0530

    eth send fix in example

--- examples/vue-app/src/lib/eth.ts
+++ examples/vue-app/src/lib/eth.ts
@@ -1,20 +1,21 @@
 import { SafeEventEmitterProvider } from "@web3auth/base";
 import Web3 from "web3";
 
-export const sendEth = async (provider: SafeEventEmitterProvider, console: any) => {
+export const sendEth = async (provider: SafeEventEmitterProvider, uiConsole: any) => {
   try {
-    const pubKey = await provider.request({ method: "eth_accounts" })
-    console.log("pubKey", pubKey)
+    
     const web3 = new Web3(provider as any);
-    const blockNumber = await provider.request({ method: "eth_getBlockByNumber", params: ["latest", false] })
-    const txRes = await web3.eth.sendTransaction({ from: "0x2c7536E3605D9C16a7a3D7b1898e529396a65c23", to: pubKey[0], value: web3.utils.toWei("0.01") })
-    console("txRes", txRes)
+    const accounts = await web3.eth.getAccounts();
+    console.log("pubKey", accounts)
+    const txRes = await web3.eth.sendTransaction({ from: accounts[0], to: accounts[0], value: web3.utils.toWei("0.01") })
+    uiConsole("txRes", txRes)
   } catch (error) {
-    console("error", error)
+    console.log("error", error)
+    uiConsole("error", error)
   }
 }
 
-export const signEthMessage =  async (provider: SafeEventEmitterProvider, console: any) =>  {
+export const signEthMessage =  async (provider: SafeEventEmitterProvider, uiConsole: any) =>  {
   try {
     const pubKey = await provider.request({ method: "eth_accounts" })
     const web3 = new Web3();
@@ -29,12 +30,13 @@ export const signEthMessage =  async (provider: SafeEventEmitterProvider, consol
       },
       (err: Error, result: any) => {
         if (err) {
-          return console(err);
+          return uiConsole(err);
         }
-        console("sign message => true", result);
+        uiConsole("sign message => true", result);
       }
     );
   } catch (error) {
-    console("error", error)
+    console.log("error", error)
+    uiConsole("error", error)
   }
 }
