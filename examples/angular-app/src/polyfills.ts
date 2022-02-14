3eac259f432f24671b96959c627f8db5c89f2cb3
tai.nguyen <tai.tan849@gmail.com>
2022-02-14 18:02:05 +0700

    Fix Webpack 5 breaking changes

--- examples/angular-app/src/polyfills.ts
+++ examples/angular-app/src/polyfills.ts
@@ -47,6 +47,9 @@
  */
 import 'zone.js';  // Included with Angular CLI.
 
+(window as any).global = window;
+global.Buffer = global.Buffer || require('buffer').Buffer;
+global.process = global.process || require('process');
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
