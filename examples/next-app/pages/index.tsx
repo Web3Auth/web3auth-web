863052b96fc4b3805d95c84748e763aae982f0f1
chaitanyapotti <chaitanya.potti@gmail.com>
2022-02-14 18:23:19 +0800

    next app refactor

--- examples/next-app/pages/index.tsx
+++ examples/next-app/pages/index.tsx
@@ -1,106 +1,29 @@
+import type { NextPage } from "next";
 
-import type { Web3Auth } from '@web3auth/web3auth'
-import { ADAPTER_EVENTS, CHAIN_NAMESPACES } from '@web3auth/base'
-
-import type { NextPage } from 'next'
-
-import { useState, useEffect } from 'react'
+import { useState, Suspense, ChangeEvent } from "react";
+import { Web3AuthProvider } from "../services/web3auth";
+import { WEB3AUTH_NETWORK, WEB3AUTH_NETWORK_TYPE } from "../config/web3AuthNetwork";
+import { CHAIN_CONFIG_TYPE } from "../config/chainConfig";
 
 const Home: NextPage = () => {
-  const [user, setUser] = useState(null);
-  const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    console.log("useEffect");
-
-    const subscribeAuthEvents = (web3auth: Web3Auth) => {
-      web3auth.on(ADAPTER_EVENTS.CONNECTED, (data) => {
-        console.log("Yeah!, you are successfully logged in", data);
-        setUser(data);
-      });
-
-      web3auth.on(ADAPTER_EVENTS.CONNECTING, () => {
-        console.log("connecting");
-      });
-
-      web3auth.on(ADAPTER_EVENTS.DISCONNECTED, () => {
-        console.log("disconnected");
-        setUser(null);
-      });
-
-      web3auth.on(ADAPTER_EVENTS.ERRORED, (error) => {
-        console.log("some error or user have cancelled login request", error);
-      });
-    };
-
-    const polygonMumbaiConfig = {
-      chainNamespace: CHAIN_NAMESPACES.EIP155,
-      chainId: "0x1",
-    };
-
-   
-
-    // ⭐️ initialize modal on page mount.
-    const initializeModal = async () => {
-      const { Web3Auth } = (await import("@web3auth/web3auth"));
-      const web3auth = new Web3Auth({
-        chainConfig: polygonMumbaiConfig,
-        clientId: "BKPxkCtfC9gZ5dj-eg-W6yb5Xfr3XkxHuGZl2o2Bn8gKQ7UYike9Dh6c-_LaXlUN77x0cBoPwcSx-IVm0llVsLA",
-      });
-      setWeb3auth(web3auth);
-      console.log("initializeModal");
-      subscribeAuthEvents(web3auth);
-      await web3auth.initModal();
-      setLoaded(true);
-    };
-
-    initializeModal();
-  }, []);
-
-  const login = async () => {
-    try {
-      if (!web3auth) return;
-      const provider = await web3auth.connect();
-      // TODO: add this provider to web3/ethers
-     } catch (error) {
-       console.log("error in login", error);
-     }
+  const [web3AuthNetwork, setWeb3AuthNetwork] = useState<WEB3AUTH_NETWORK_TYPE>("mainnet");
+  const [chain, setChain] = useState<CHAIN_CONFIG_TYPE>("mainnet");
+  const web3AuthChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
+    setWeb3AuthNetwork(e.target.value as WEB3AUTH_NETWORK_TYPE);
   };
-  const logout = async () => {
-    if (!web3auth) return;
-    await web3auth.logout();
-  };
-  const getUserInfo = async () => {
-    if (!web3auth) return;
-    const userInfo = await web3auth.getUserInfo();
-    console.log(userInfo);
-  };
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Web3AuthProvider chain={chain} web3AuthNetwork={web3AuthNetwork}>
+        <h1>Hello World</h1>
+        <select onChange={web3AuthChangeHandler}>
+          {Object.keys(WEB3AUTH_NETWORK).map((x: string) => {
+            return <option value={x}>{WEB3AUTH_NETWORK[x as WEB3AUTH_NETWORK_TYPE].displayName}</option>;
+          })}
+        </select>
+      </Web3AuthProvider>
+    </Suspense>
+  );
+};
 
-  const renderUnauthenticated = () => {
-    return (
-      <div className="main">
-        <button className="app-link" onClick={login}>
-          LOGIN
-        </button>
-      </div>
-    );
-  };
-
-  const renderAuthenticated = () => {
-    return (
-      <div className="App">
-        <button className="app-link" onClick={logout}>
-          LOG OUT
-        </button>
-        <button className="app-link" onClick={getUserInfo}>
-          Log user info
-        </button>
-      </div>
-    );
-  };
-
-  return loaded ? (user ? renderAuthenticated() : renderUnauthenticated()): <h1>Loading....</h1>;
-}
-
-export default Home
+export default Home;
---
+++ examples/next-app/pages/login.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import { useState, useEffect } from "react";
+
+const Home: NextPage = () => {
+  return <h1>Loading....</h1>;
+};
+
+export default Home;
