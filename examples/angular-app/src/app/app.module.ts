92a0ed22a1cec409d87b62a88ba02fa4f040c607
tai.nguyen <tai.tan849@gmail.com>
2022-02-18 00:25:13 +0700

    add footer reference to repo

--- examples/angular-app/src/app/app.module.ts
+++ examples/angular-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SettingComponent } from './setting/setting.component';
 import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingComponent,
     MainComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
