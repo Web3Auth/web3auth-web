8ffc0ed777e7ddc8c590fed641b86319df0f41bc
Naveed Elahi <statix231@hotmail.com>
2022-02-21 16:45:39 +0600

    add react example

---
+++ examples/react-app/src/components/Main.tsx
@@ -0,0 +1,40 @@
+import { useWeb3Auth } from "../services/web3auth";
+import styles from "../styles/Home.module.css";
+
+const Main = () => {
+  const { provider, login, logout, getUserInfo, getAccounts, getBalance, signMessage } = useWeb3Auth();
+
+  const loggedInView = (
+    <>
+      <a onClick={getUserInfo} className={styles.card}>
+        Get User Info
+      </a>
+      <a onClick={getAccounts} className={styles.card}>
+        Get Accounts
+      </a>
+      <a onClick={getBalance} className={styles.card}>
+        Get Balance
+      </a>
+      <a onClick={signMessage} className={styles.card}>
+        Sign Message
+      </a>
+      <a onClick={logout} className={styles.card}>
+        Log Out
+      </a>
+
+      <div className={styles.console} id="console">
+        <p className={styles.code}></p>
+      </div>
+    </>
+  );
+
+  const unloggedInView = (
+    <a onClick={login} className={styles.card}>
+      Login
+    </a>
+  );
+
+  return <div className={styles.grid}>{provider ? loggedInView : unloggedInView}</div>;
+};
+
+export default Main;
---
+++ examples/react-app/src/services/web3auth.tsx
@@ -0,0 +1,182 @@
+import { ADAPTER_EVENTS, SafeEventEmitterProvider } from "@web3auth/base";
+import type { Web3Auth } from "@web3auth/web3auth";
+import { createContext, FunctionComponent, ReactNode, useContext, useEffect, useState } from "react";
+import { CHAIN_CONFIG, CHAIN_CONFIG_TYPE } from "../config/chainConfig";
+import { WEB3AUTH_NETWORK_TYPE } from "../config/web3AuthNetwork";
+import { getWalletProvider, IWalletProvider } from "./walletProvider";
+
+export interface IWeb3AuthContext {
+  web3Auth: Web3Auth | null;
+  provider: SafeEventEmitterProvider | null;
+  isLoading: boolean;
+  user: unknown;
+  login: () => Promise<void>;
+  logout: () => Promise<void>;
+  getUserInfo: () => Promise<any>;
+  signMessage: () => Promise<any>;
+  getAccounts: () => Promise<any>;
+  getBalance: () => Promise<any>;
+}
+
+export const Web3AuthContext = createContext<IWeb3AuthContext>({
+  web3Auth: null,
+  provider: null,
+  isLoading: false,
+  user: null,
+  login: async () => {},
+  logout: async () => {},
+  getUserInfo: async () => {},
+  signMessage: async () => {},
+  getAccounts: async () => {},
+  getBalance: async () => {},
+});
+
+export function useWeb3Auth() {
+  return useContext(Web3AuthContext);
+}
+
+interface IWeb3AuthState {
+  web3AuthNetwork: WEB3AUTH_NETWORK_TYPE;
+  chain: CHAIN_CONFIG_TYPE;
+}
+interface IWeb3AuthProps {
+  children?: ReactNode;
+  web3AuthNetwork: WEB3AUTH_NETWORK_TYPE;
+  chain: CHAIN_CONFIG_TYPE;
+}
+
+export const Web3AuthProvider: FunctionComponent<IWeb3AuthState> = ({ children, web3AuthNetwork, chain }: IWeb3AuthProps) => {
+  const [web3Auth, setWeb3Auth] = useState<Web3Auth | null>(null);
+  const [provider, setProvider] = useState<IWalletProvider | null>(null);
+  const [user, setUser] = useState<unknown | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const subscribeAuthEvents = (web3auth: Web3Auth) => {
+      // Can subscribe to all ADAPTER_EVENTS and LOGIN_MODAL_EVENTS
+      web3auth.on(ADAPTER_EVENTS.CONNECTED, (data: unknown) => {
+        console.log("Yeah!, you are successfully logged in", data);
+        setUser(data);
+        setWalletProvider(web3auth.provider!);
+      });
+
+      web3auth.on(ADAPTER_EVENTS.CONNECTING, () => {
+        console.log("connecting");
+      });
+
+      web3auth.on(ADAPTER_EVENTS.DISCONNECTED, () => {
+        console.log("disconnected");
+        setUser(null);
+      });
+
+      web3auth.on(ADAPTER_EVENTS.ERRORED, (error) => {
+        console.error("some error or user has cancelled login request", error);
+      });
+    };
+
+    const currentChainConfig = CHAIN_CONFIG[chain];
+
+    async function init() {
+      try {
+        const { Web3Auth } = await import("@web3auth/web3auth");
+        setIsLoading(true);
+        const web3AuthInstance = new Web3Auth({
+          chainConfig: currentChainConfig,
+          // get your client id from https://dashboard.web3auth.io
+          clientId: "BKPxkCtfC9gZ5dj-eg-W6yb5Xfr3XkxHuGZl2o2Bn8gKQ7UYike9Dh6c-_LaXlUN77x0cBoPwcSx-IVm0llVsLA",
+        });
+        subscribeAuthEvents(web3AuthInstance);
+        setWeb3Auth(web3AuthInstance);
+        await web3AuthInstance.initModal();
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    init();
+  }, [chain, web3AuthNetwork]);
+
+  const login = async () => {
+    if (!web3Auth) {
+      console.log("web3auth not initialized yet");
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const localProvider = await web3Auth.connect();
+    setWalletProvider(localProvider!);
+  };
+
+  const setWalletProvider = (web3authProvider: SafeEventEmitterProvider) => {
+    const walletProvider = getWalletProvider(chain, web3authProvider, uiConsole);
+    setProvider(walletProvider);
+  };
+
+  const logout = async () => {
+    if (!web3Auth) {
+      console.log("web3auth not initialized yet");
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3Auth.logout();
+    setProvider(null);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3Auth) {
+      console.log("web3auth not initialized yet");
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3Auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    provider.getAccounts();
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    provider.getBalance();
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      console.log("provider not initialized yet");
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    provider.signMessage();
+  };
+
+  const uiConsole = (...args: unknown[]): void => {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  };
+
+  const contextProvider = {
+    web3Auth,
+    provider,
+    user,
+    isLoading,
+    login,
+    logout,
+    getUserInfo,
+    getAccounts,
+    getBalance,
+    signMessage,
+  };
+  return <Web3AuthContext.Provider value={contextProvider}>{children}</Web3AuthContext.Provider>;
+};
