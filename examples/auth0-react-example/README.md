d60df37d6ebe79875275b018a47f23a4c6e5bd36
himanshuchawla009 <himanshuchawla2014@gmail.com>
2022-05-10 01:51:18 +0530

    auth0 working example

---
+++ examples/auth0-react-example/README.md
@@ -0,0 +1,2 @@
+REACT_APP_CLIENT_ID="BKPxkCtfC9gZ5dj-eg-W6yb5Xfr3XkxHuGZl2o2Bn8gKQ7UYike9Dh6c-_LaXlUN77x0cBoPwcSx-IVm0llVsLA"
+REACT_APP_DOMAIN="https://torus-test.auth0.com"
---
+++ examples/auth0-react-example/src/config/chainConfig.ts
@@ -0,0 +1,33 @@
+import { CHAIN_NAMESPACES, CustomChainConfig } from "@web3auth/base";
+
+export const CHAIN_CONFIG = {
+  mainnet: {
+    displayName: "Ethereum Mainnet",
+    chainNamespace: CHAIN_NAMESPACES.EIP155,
+    chainId: "0x1",
+    rpcTarget: `https://mainnet.infura.io/v3/776218ac4734478c90191dde8cae483c`,
+    blockExplorer: "https://etherscan.io/",
+    ticker: "ETH",
+    tickerName: "Ethereum",
+  } as CustomChainConfig,
+  solana: {
+    chainNamespace: CHAIN_NAMESPACES.SOLANA,
+    rpcTarget: "https://api.mainnet-beta.solana.com",
+    blockExplorer: "https://explorer.solana.com/",
+    chainId: "0x1",
+    displayName: "Solana Mainnet",
+    ticker: "SOL",
+    tickerName: "Solana",
+  } as CustomChainConfig,
+  polygon: {
+    chainNamespace: CHAIN_NAMESPACES.EIP155,
+    rpcTarget: "https://polygon-rpc.com",
+    blockExplorer: "https://polygonscan.com/",
+    chainId: "0x89",
+    displayName: "Polygon Mainnet",
+    ticker: "matic",
+    tickerName: "Matic",
+  } as CustomChainConfig,
+} as const;
+
+export type CHAIN_CONFIG_TYPE = keyof typeof CHAIN_CONFIG;
