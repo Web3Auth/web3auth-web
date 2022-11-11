f9a138c16d9ca6757bc55de1579f33ae0d9f8642
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2022-11-11 13:03:16 +0530

    Update react app

--- demo/react-app/src/index.tsx
+++ demo/react-app/src/index.tsx
@@ -1,14 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import { createRoot } from 'react-dom/client'
 import "./index.css";
 import App from "./App";
 
-const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container);
-
-
-root.render(
-  <StrictMode>
+createRoot(
+  document.getElementById('root') as HTMLElement)
+  .render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+  </React.StrictMode>
+)
