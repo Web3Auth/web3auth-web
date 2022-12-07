92c667451ee164969ab80e99393415b0a431fc2c
Saviour1001 <harshghodkar@gmail.com>
2022-12-08 00:50:48 +0530

    shifted some demos to examples repos

--- demo/firebase-next-app/config/web3AuthNetwork.ts
+++
@@ -1,7 +0,0 @@
-export const WEB3AUTH_NETWORK = {
-  testnet: {
-    displayName: "Testnet",
-  }
-} as const;
-
-export type WEB3AUTH_NETWORK_TYPE = keyof typeof WEB3AUTH_NETWORK;
--- demo/next-app/components/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from "react";
-
-const Loader: FunctionComponent = () => {
-  return <h1>Loading....</h1>;
-};
-
-export default Loader;
