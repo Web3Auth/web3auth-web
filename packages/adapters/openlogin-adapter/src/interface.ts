ec0ae85737c012141ae1241bba974c16bc7d4b81
himanshuchawla009 <himanshuchawla2014@gmail.com>
2023-01-16 12:00:13 +0530

    minor comment

--- packages/adapters/openlogin-adapter/src/interface.ts
+++ packages/adapters/openlogin-adapter/src/interface.ts
@@ -3,6 +3,12 @@ import { BaseAdapterSettings } from "@web3auth/base";
 
 export type LoginSettings = LoginParams &
   Partial<BaseRedirectParams> & {
+    /**
+     * Set this param to true if user is created user web3auth's sfa sdks.
+     * for ex: `@web3auth/single-factor-auth`
+     *
+     * @defaultValue false
+     */
     isSfaUser?: boolean;
   };
 
