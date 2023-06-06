a665449c553067910cd8846c3ed723cf8a68ddcd
chaitanyapotti <chaitanya.potti@gmail.com>
2023-06-06 11:29:29 +0530

    fix lock file + interface

--- packages/adapters/openlogin-adapter/src/interface.ts
+++ packages/adapters/openlogin-adapter/src/interface.ts
@@ -7,10 +7,11 @@ export type LoginSettings = Partial<LoginParams> & Partial<BaseRedirectParams>;
 type MakeOptional<Type, Key extends keyof Type> = Omit<Type, Key> & Partial<Pick<Type, Key>>;
 
 export type PrivateKeyProvider = IBaseProvider<string>;
+
 export interface OpenloginAdapterOptions extends BaseAdapterSettings {
   adapterSettings?: MakeOptional<OpenLoginOptions, "clientId" | "network"> & {
     useCoreKitKey?: boolean;
-    provider?: PrivateKeyProvider;
+    privateKeyProvider?: PrivateKeyProvider;
   };
   loginSettings?: LoginSettings;
 }
