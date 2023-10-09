e822b23d4078f1817099c97d92561c560e2f4af7
Lionell Briones <llenoil@gmail.com>
2023-10-09 14:44:06 +0800

    instance use

--- packages/ui/src/localeImport.ts
+++ packages/ui/src/localeImport.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from "react-i18next";
 import { en } from "./i18n";
 
 const i18nInstance = i18n.createInstance();
-i18n.use(initReactI18next).init({
+i18nInstance.use(initReactI18next).init({
   resources: {
     en: { translation: en },
   },
