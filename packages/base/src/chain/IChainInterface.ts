22df6cf0967c5f4feef78cae08b0bafeab70bf68
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-02-07 12:43:03 +0800

    add casper chain namespace

--- packages/base/src/chain/IChainInterface.ts
+++ packages/base/src/chain/IChainInterface.ts
@@ -1,6 +1,7 @@
 export const CHAIN_NAMESPACES = {
   EIP155: "eip155",
   SOLANA: "solana",
+  CASPER: "casper",
   XRPL: "xrpl",
   OTHER: "other",
 } as const;
@@ -10,6 +11,7 @@ export type ChainNamespaceType = (typeof CHAIN_NAMESPACES)[keyof typeof CHAIN_NA
 export const ADAPTER_NAMESPACES = {
   EIP155: "eip155",
   SOLANA: "solana",
+  CASPER: "casper",
   XRPL: "xrpl",
   MULTICHAIN: "multichain",
 } as const;
