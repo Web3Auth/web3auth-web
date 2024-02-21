7e27ed05c2ba8fd03e0a9f81829f163a6eaf9b35
Lionell Briones <llenoil@gmail.com>
2024-02-21 22:21:36 +0800

    use cdn for footer images

--- packages/ui/src/components/SelfCustodyViaWeb3Auth.tsx
+++ packages/ui/src/components/SelfCustodyViaWeb3Auth.tsx
@@ -1,7 +1,5 @@
 import { useTranslation } from "react-i18next";
 
-import Web3AuthLogoDark from "../../assets/web3auth-logo-dark.svg";
-import Web3AuthLogoLight from "../../assets/web3auth-logo-light.svg";
 import i18n from "../localeImport";
 
 export default function SelfCustodyViaWeb3Auth() {
@@ -10,8 +8,8 @@ export default function SelfCustodyViaWeb3Auth() {
   return (
     <div className="flex items-center gap-2 justify-center p-2">
       <div className="text-xs text-app-gray-300 dark:text-app-gray-500">{t("modal.footer.message-new")}</div>
-      <img height="16" src={Web3AuthLogoLight} alt="Web3Auth Logo Light" className="h-4 block dark:hidden" />
-      <img height="16" src={Web3AuthLogoDark} alt="Web3Auth Logo Dark" className="h-4 hidden dark:block" />
+      <img height="16" src="https://images.web3auth.io/web3auth-footer-logo-light.svg" alt="Web3Auth Logo Light" className="h-4 block dark:hidden" />
+      <img height="16" src="https://images.web3auth.io/web3auth-footer-logo-dark.svg" alt="Web3Auth Logo Dark" className="h-4 hidden dark:block" />
     </div>
   );
 }
