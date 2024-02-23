3096c1c69b48fb38e2b5aeef0e2bd4682c029cea
Archit <arch1995@gmail.com>
2024-02-23 16:34:21 +0530

    adds plugin events

--- packages/plugins/base-plugin/src/IPlugin.ts
+++ packages/plugins/base-plugin/src/IPlugin.ts
@@ -5,7 +5,32 @@ export const PLUGIN_NAMESPACES = {
   MULTICHAIN: "multichain",
 } as const;
 
+export const PLUGIN_STATUS = {
+  READY: "ready",
+  CONNECTING: "connecting",
+  CONNECTED: "connected",
+  DISCONNECTED: "disconnected",
+  ERRORED: "errored",
+} as const;
+
+export const PLUGIN_EVENTS = {
+  ...PLUGIN_STATUS,
+} as const;
+
 export type PluginNamespace = (typeof PLUGIN_NAMESPACES)[keyof typeof PLUGIN_NAMESPACES];
+
+export const EVM_PLUGINS = {
+  WALLET_SERVICES: "wallet-services",
+} as const;
+
+export const SOLANA_PLUGINS = {
+  SOLANA: "solana",
+} as const;
+
+export const WALLET_PLUGINS = {
+  ...EVM_PLUGINS,
+  ...SOLANA_PLUGINS,
+} as const;
 
 export interface IPlugin {
   name: string;
