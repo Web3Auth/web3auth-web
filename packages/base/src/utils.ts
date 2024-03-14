06c5edb6b2e0fc729b1227c96427fad6438fcb50
Naveed Elahi <statix231@hotmail.com>
2024-03-14 18:19:51 +0800

    move fetch project config to @web3auth/base

--- packages/base/src/utils.ts
+++ packages/base/src/utils.ts
@@ -1,3 +1,16 @@
+import { SIGNER_MAP } from "@toruslabs/constants";
+import { get } from "@toruslabs/http-helpers";
+import type { OPENLOGIN_NETWORK_TYPE, WhiteLabelData } from "@toruslabs/openlogin-utils";
+
+import { WEB3AUTH_NETWORK } from "./adapter";
+
+interface PROJECT_CONFIG_RESPONSE {
+  whitelabel?: WhiteLabelData;
+  sms_otp_enabled: boolean;
+  wallet_connect_enabled: boolean;
+  wallet_connect_project_id?: string;
+}
+
 export function storageAvailable(type: "sessionStorage" | "localStorage"): boolean {
   let storageExists = false;
   let storageLength = 0;
@@ -32,4 +45,19 @@ export function storageAvailable(type: "sessionStorage" | "localStorage"): boole
 
 export const isHexStrict = (hex: string): boolean => {
   return (typeof hex === "string" || typeof hex === "number") && /^(-)?0x[0-9a-f]*$/i.test(hex);
+};
+
+export const signerHost = (web3AuthNetwork?: OPENLOGIN_NETWORK_TYPE): string => {
+  return SIGNER_MAP[web3AuthNetwork ?? WEB3AUTH_NETWORK.SAPPHIRE_MAINNET];
+};
+
+export const fetchProjectConfig = async (clientId: string, web3AuthNetwork?: OPENLOGIN_NETWORK_TYPE): Promise<PROJECT_CONFIG_RESPONSE> => {
+  try {
+    const url = new URL(`${signerHost(web3AuthNetwork)}/api/configuration`);
+    url.searchParams.append("project_id", clientId);
+    const res = await get<PROJECT_CONFIG_RESPONSE>(String(url));
+    return res;
+  } catch (e) {
+    throw new Error(`Failed to fetch project config: ${(e as Error).message}`);
+  }
 };
