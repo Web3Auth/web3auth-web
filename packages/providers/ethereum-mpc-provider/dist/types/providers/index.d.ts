e4a22e7ad667fea66e5caa962dfbf337254dfdbb
ieow <cwlee0@gmail.com>
2024-04-08 20:41:58 +0800

    rename package

--- packages/providers/ethereum-mpc-provider/dist/types/providers/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./signingProviders";
-export * from "./TransactionFormatter";
--- packages/providers/ethereum-mpc-provider/dist/types/rpc/walletMidddleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { JRPCMiddleware } from "@toruslabs/openlogin-jrpc";
-import type { WalletMiddlewareOptions } from "./interfaces";
-export declare function createWalletMiddleware({ getAccounts, getPrivateKey, processDecryptMessage, processEncryptionPublicKey, processEthSignMessage, processPersonalMessage, processTransaction, processSignTransaction, processTypedMessage, processTypedMessageV3, processTypedMessageV4, }: WalletMiddlewareOptions): JRPCMiddleware<string, unknown>;
