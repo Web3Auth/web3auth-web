3dbc330943eb82df1a5629a84927ed542f1d4c0c
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-04-11 18:23:10 +0800

    fix hooks build

--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  presets: ["@babel/env", ["@babel/preset-react", { runtime: "automatic" }], "@babel/typescript"],
+  presets: [["@babel/preset-react", { runtime: "automatic" }]],
 };
