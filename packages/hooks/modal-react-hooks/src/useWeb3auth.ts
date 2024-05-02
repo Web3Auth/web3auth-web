1740b63c1fc42548357d0bb3b9d6c3ec6208ce26
Archit <arch1995@gmail.com>
2024-05-02 12:46:19 +0800

    fixes isMfaEnabled set instantly on redirect mode

--- packages/hooks/modal-react-hooks/src/useWeb3auth.ts
+++ packages/hooks/modal-react-hooks/src/useWeb3auth.ts
@@ -82,7 +82,13 @@ export const useWeb3Auth = () => {
       if (!web3auth) throw WalletInitializationError.notReady();
       if (!isConnected) throw WalletLoginError.notConnectedError();
       await web3auth.enableMFA(params);
-      setIsMFAEnabled(true);
+      // in case of redirect mode, enableMFA will not return anything
+      // and we could set isMFAEnabled to true instantly even if the user
+      // is being redirected to the enable MFA screens.
+      // so instant of updating directly to true, we will check the user info.
+      const localUserInfo = await web3auth.getUserInfo();
+      setUserInfo(localUserInfo);
+      setIsMFAEnabled(localUserInfo.isMfaEnabled || false);
     },
     [web3auth, isConnected]
   );
