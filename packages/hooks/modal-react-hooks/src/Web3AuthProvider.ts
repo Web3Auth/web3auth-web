cc799e56abff1b262a3edc774afaf459f2fbb0bb
Archit <arch1995@gmail.com>
2024-05-07 22:31:46 +0800

    code split up

--- packages/hooks/modal-react-hooks/src/Web3AuthProvider.ts
+++ packages/hooks/modal-react-hooks/src/Web3AuthProvider.ts
@@ -5,5 +5,6 @@ import { Web3AuthInnerProvider } from "./context/Web3AuthInnerContext";
 import { Web3AuthProviderProps } from "./interfaces";
 
 export function Web3AuthProvider({ config, children }: PropsWithChildren<Web3AuthProviderProps>) {
-  return createElement(Web3AuthInnerProvider, { config }, createElement(WalletServicesProvider, null, children));
+  const wsElement = createElement(WalletServicesProvider, {}, children);
+  return createElement(Web3AuthInnerProvider, { config }, wsElement);
 }
