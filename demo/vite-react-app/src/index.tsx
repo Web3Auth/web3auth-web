b14f0d77a82d8d8a1412cc08af7f9ee7daa10096
Tomas Rawski <tomas@tor.us>
2024-05-14 06:10:52 -0300

    Feat/add vite react demo (#1808)
    * add vite react demo
    
    * Fix wallet initialization
    
    * fix Solana RPC,read tokens and write to smart contract
    
    * fix fn write contract and improve fn to get the address
    
    * add arbitrum logo
    
    * nit for prod testing
    
    ---------
    
    Co-authored-by: Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>

---
+++ demo/vite-react-app/src/index.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
---
+++ demo/vite-react-app/src/services/walletProvider.ts
@@ -0,0 +1,25 @@
+import { IProvider } from "@web3auth/base";
+import ethProvider from "./ethProvider";
+import solanaProvider from "./solanaProvider";
+// import tezosProvider from "./tezosProvider";
+
+export interface IWalletProvider {
+  getAccounts: () => Promise<any>;
+  getBalance: () => Promise<any>;
+  signAndSendTransaction: () => Promise<void>;
+  signTransaction: () => Promise<void>;
+  signMessage: () => Promise<void>;
+  getTokenBalance?: () => Promise<void>;
+  signAndSendTokenTransaction?: () => Promise<void>;
+  randomContractInteraction?: () => Promise<void>;
+}
+
+export const getWalletProvider = (chain: string, provider: IProvider, uiConsole: any): IWalletProvider => {
+  if (chain === "solana") {
+    return solanaProvider(provider, uiConsole);
+  }
+  // } else if (chain === "tezos") {
+  //   return tezosProvider(provider, uiConsole);
+  // }
+  return ethProvider(provider, uiConsole);
+};
