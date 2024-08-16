fa820ecf8c9c9d6073388667dc0b343c0554b74a
hieu-w <hieu@tor.us>
2024-08-16 09:52:49 +0700

    Fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,15 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  extends: ["@toruslabs/vue"],
-  parser: "vue-eslint-parser",
+  extends: ["@toruslabs/eslint-config-typescript"],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["*.config.js", ".eslintrc.js"],
   parserOptions: {
-    parser: "@typescript-eslint/parser",
     sourceType: "module",
     ecmaVersion: 2022,
     project: "./tsconfig.json",
   },
-  ignorePatterns: ["*.config.js", "*.d.ts", ".eslintrc.js"],
-  env: {
-    es2020: true,
-    browser: true,
-    node: true,
-    mocha: true,
-  },
   rules: {
     camelcase: 0,
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
-      },
-    ],
-  }
+  },
 };
