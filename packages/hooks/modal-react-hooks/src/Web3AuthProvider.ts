73c3707ab1eb075c6db98c10da8f0451899631f6
Naveed Elahi <statix231@hotmail.com>
2024-09-04 19:53:01 +0800

    add missing modalConfig prop for Web3AuthProvider in react modal hooks

--- packages/hooks/modal-react-hooks/src/Web3AuthProvider.ts
+++ packages/hooks/modal-react-hooks/src/Web3AuthProvider.ts
@@ -3,6 +3,6 @@ import { createElement, PropsWithChildren } from "react";
 import { Web3AuthInnerProvider } from "./context/Web3AuthInnerContext";
 import { Web3AuthProviderProps } from "./interfaces";
 
-export function Web3AuthProvider({ config, children }: PropsWithChildren<Web3AuthProviderProps>) {
-  return createElement(Web3AuthInnerProvider, { config }, children);
+export function Web3AuthProvider({ config, modalConfig, children }: PropsWithChildren<Web3AuthProviderProps>) {
+  return createElement(Web3AuthInnerProvider, { config, modalConfig }, children);
 }
