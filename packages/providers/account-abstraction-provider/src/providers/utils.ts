24c3488aa755146960a13086e4b8631b42be636a
Nguyen Anh Tu <tunguyen.bhtech@gmail.com>
2024-09-24 09:30:41 +0700

    feat(app): aa - send transaction polling with timeout

--- packages/providers/account-abstraction-provider/src/providers/utils.ts
+++ packages/providers/account-abstraction-provider/src/providers/utils.ts
@@ -7,6 +7,48 @@ import { TypedDataEncoder } from "ethers";
 import { Chain, createWalletClient, Hex, http } from "viem";
 import { BundlerClient, SendUserOperationParameters, SmartAccount } from "viem/account-abstraction";
 
+type PollOptions<data> = {
+  // Whether or not to emit when the polling starts.
+  emitOnBegin?: boolean | undefined;
+  // The initial wait time (in ms) before polling.
+  initialWaitTime?: ((data: data | void) => Promise<number>) | undefined;
+  // The interval (in ms).
+  interval: number;
+};
+
+/**
+ * @description Polls a function at a specified interval.
+ * same poll function as viem/utils/poll
+ */
+export function poll<data>(
+  fn: ({ unpoll }: { unpoll: () => void }) => Promise<data | void>,
+  { emitOnBegin, initialWaitTime, interval }: PollOptions<data>
+) {
+  let active = true;
+
+  const unwatch = () => (active = false);
+
+  const watch = async () => {
+    let data: data | void;
+    if (emitOnBegin) data = await fn({ unpoll: unwatch });
+
+    const initialWait = (await initialWaitTime?.(data)) ?? interval;
+    await sleep(initialWait);
+
+    const _poll = async () => {
+      if (!active) return;
+      await fn({ unpoll: unwatch });
+      await sleep(interval);
+      _poll();
+    };
+
+    _poll();
+  };
+  watch();
+
+  return unwatch;
+}
+
 export function getProviderHandlers({
   bundlerClient,
   smartAccount,
@@ -56,21 +98,44 @@ export function getProviderHandlers({
       };
       // @ts-expect-error viem types are too deep
       const userOpHash = await bundlerClient.sendUserOperation(userOperationParams);
-      while (true) {
-        // keep checking for user operation until it is online to return the transaction hash
-        // without needing to wait for the receipt
-        try {
-          const receipt = await bundlerClient.getUserOperation({ hash: userOpHash });
-          return receipt.transactionHash;
-        } catch (error) {
-          if (error instanceof Error && error.message.toLowerCase().includes("could not be found")) {
-            await sleep(1000);
-            continue;
-          } else {
-            throw error;
+
+      const timeout = 120_000;
+      return new Promise((resolve, reject) => {
+        const done = (fn: () => void) => {
+          // eslint-disable-next-line @typescript-eslint/no-use-before-define, no-use-before-define
+          unpoll();
+          fn();
+        };
+
+        const unpoll = poll(
+          async () => {
+            // keep checking for user operation until it is online to return the transaction hash
+            // without needing to wait for the receipt
+            try {
+              const receipt = await bundlerClient.getUserOperation({ hash: userOpHash });
+              done(() => resolve(receipt.transactionHash));
+            } catch (error) {
+              if (!(error instanceof Error && error.message.toLowerCase().includes("could not be found"))) {
+                done(() => reject(error));
+              }
+            }
+          },
+          {
+            interval: 1000,
           }
-        }
-      }
+        );
+
+        setTimeout(() => {
+          done(() =>
+            reject(
+              providerErrors.custom({
+                message: "Process transaction wait timeout.",
+                code: 4904,
+              })
+            )
+          );
+        }, timeout);
+      });
     },
     processSignTransaction: async (txParams: TransactionParams): Promise<string> => {
       const { to, value, data, maxFeePerGas, maxPriorityFeePerGas } = txParams;
