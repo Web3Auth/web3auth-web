55ac37b139df75aa786d5cbcaf435fa0c8a5c003
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-09-26 17:10:14 +0800

    make transport optional

--- packages/providers/account-abstraction-provider/src/providers/types.ts
+++ packages/providers/account-abstraction-provider/src/providers/types.ts
@@ -2,8 +2,10 @@ import { createBundlerClient, createPaymasterClient } from "viem/account-abstrac
 
 export type BundlerConfig = {
   url: string;
-} & Pick<Parameters<typeof createBundlerClient>[0], "key" | "name" | "cacheTime" | "pollingInterval" | "userOperation" | "rpcSchema" | "transport">;
+} & Pick<Parameters<typeof createBundlerClient>[0], "key" | "name" | "cacheTime" | "pollingInterval" | "userOperation" | "rpcSchema"> &
+  Partial<Pick<Parameters<typeof createBundlerClient>[0], "transport">>;
 
 export type PaymasterConfig = {
   url: string;
-} & Pick<Parameters<typeof createPaymasterClient>[0], "key" | "name" | "pollingInterval" | "rpcSchema" | "cacheTime" | "transport">;
+} & Pick<Parameters<typeof createPaymasterClient>[0], "key" | "name" | "pollingInterval" | "rpcSchema" | "cacheTime"> &
+  Partial<Pick<Parameters<typeof createPaymasterClient>[0], "transport">>;
