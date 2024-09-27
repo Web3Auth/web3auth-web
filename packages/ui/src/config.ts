30c28ee83f610697bb539b41f7b8d88d498330b1
Chaitanya Potti <chaitanya.potti@gmail.com>
2024-09-27 13:11:42 +0800

    restrict passkeys, authenticator login methods

--- packages/ui/src/config.ts
+++ packages/ui/src/config.ts
@@ -4,7 +4,13 @@ export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export const AUTH_PROVIDERS = Object.values(LOGIN_PROVIDER).filter((x) => x !== LOGIN_PROVIDER.WEBAUTHN && x !== LOGIN_PROVIDER.JWT);
+const restrictedLoginMethods: Set<LOGIN_PROVIDER_TYPE> = new Set([
+  LOGIN_PROVIDER.AUTHENTICATOR,
+  LOGIN_PROVIDER.PASSKEYS,
+  LOGIN_PROVIDER.JWT,
+  LOGIN_PROVIDER.WEBAUTHN,
+]);
+export const AUTH_PROVIDERS = Object.values(LOGIN_PROVIDER).filter((x) => !restrictedLoginMethods.has(x));
 
 export const AUTH_PROVIDERS_NAMES = AUTH_PROVIDERS.reduce(
   (acc, x) => {
