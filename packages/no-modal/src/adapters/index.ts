5c04eb14bdf147cfa86f271ffdcafe8d16012835
Archit <arch1995@gmail.com>
2025-02-11 15:41:22 +0530

    removes torus wallet adapter

--- packages/no-modal/src/adapters/index.ts
+++ packages/no-modal/src/adapters/index.ts
@@ -4,5 +4,4 @@ export * from "./base-solana-adapter";
 export * from "./coinbase-adapter";
 export * from "./default-evm-adapter";
 export * from "./default-solana-adapter";
-export * from "./torus-evm-adapter";
 export * from "./wallet-connect-v2-adapter";
