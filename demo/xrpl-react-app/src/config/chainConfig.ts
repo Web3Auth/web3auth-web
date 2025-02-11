5fe73e11e82ddc172276bfc57d8db65e146587ba
Chaitanya Potti <chaitanya.potti@gmail.com>
2025-02-11 15:07:38 +0800

    remove demos

--- demo/xrpl-react-app/src/config/chainConfig.ts
+++
@@ -1,9 +0,0 @@
-import { getXRPLChainConfig } from "@web3auth/xrpl-provider";
-
-export const CHAIN_CONFIG = {
-  "xrpl-devnet": getXRPLChainConfig("devnet"),
-  "xrpl-testnet": getXRPLChainConfig("testnet"),
-  "xrpl-mainnet": getXRPLChainConfig("mainnet"),
-} as const;
-
-export type CHAIN_CONFIG_TYPE = keyof typeof CHAIN_CONFIG;
