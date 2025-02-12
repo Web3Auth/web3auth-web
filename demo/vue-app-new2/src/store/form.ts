fdda811b13b3056283f31af3157f8d94e435fc54
Tai Nguyen TT <tai.nguyen@tor.us>
2025-02-12 18:48:23 +0700

    add temp demo app

---
+++ demo/vue-app-new2/src/store/form.ts
@@ -0,0 +1,31 @@
+import { WEB3AUTH_NETWORK } from "@web3auth/auth";
+import { CHAIN_NAMESPACES } from "@web3auth/modal";
+import { reactive } from "vue";
+
+import { defaultLoginMethod, FormData, initWhiteLabel } from "../config";
+
+export const formDataStore = reactive<FormData>({
+  // authMode: "",
+  network: WEB3AUTH_NETWORK.TESTNET,
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chain: CHAIN_NAMESPACES.EIP155,
+  whiteLabel: {
+    enable: false,
+    config: initWhiteLabel,
+  },
+  loginProviders: [],
+  adapters: [],
+  showWalletDiscovery: true,
+  loginMethods: defaultLoginMethod,
+  walletPlugin: {
+    enable: false,
+    logoLight: "",
+    logoDark: "",
+    confirmationStrategy: "default",
+  },
+  nftCheckoutPlugin: {
+    enable: false,
+  },
+  useAccountAbstractionProvider: false,
+  useAAWithExternalWallet: true,
+});
