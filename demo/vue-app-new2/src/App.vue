4477a2f2dfb91ba6a032b241222d972b4413957a
Tai Nguyen TT <tai.nguyen@tor.us>
2025-02-26 23:17:02 +0700

    cleanup vue-app-new2 demo app

--- demo/vue-app-new2/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import Main from "./MainView.vue";
-</script>
-
-<template>
-  <Main />
-</template>
--- demo/vue-app-new2/src/plugins/iconPlugin.ts
+++
@@ -1,5 +0,0 @@
-import { createIcons } from "@toruslabs/vue-components";
-
-import Icons from "../icons";
-
-export default createIcons(Icons);
