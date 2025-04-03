bae0682503b0fe30d5e1c48dc23ef8e70fc2a1ee
Tai Nguyen TT <tai.nguyen@tor.us>
2025-04-03 22:05:02 +0700

    use sentry-loglevel plugin to format errors

--- packages/no-modal/src/base/loglevel.ts
+++ packages/no-modal/src/base/loglevel.ts
@@ -1,3 +1,14 @@
+import { LoglevelSentry } from "@toruslabs/loglevel-sentry";
 import loglevel from "loglevel";
 
-export const log = loglevel.getLogger("web3auth-logger");
+const log = loglevel.getLogger("web3auth-logger");
+// TODO: remove this once we make Sentry optional in loglevel-sentry package
+const loglevelPlugin = new LoglevelSentry({
+  getActiveSpan: () => undefined,
+  addBreadcrumb: () => {},
+  captureException: () => "",
+  getClient: (): undefined => undefined,
+});
+loglevelPlugin.install(log);
+
+export { log };
