01baa135acc6e16b393a3dc69af9401266721e30
Chaitanya Potti <chaitanya.potti@gmail.com>
2025-04-04 11:17:52 +0800

    use optional sentry

--- packages/no-modal/src/base/loglevel.ts
+++ packages/no-modal/src/base/loglevel.ts
@@ -2,13 +2,7 @@ import { LoglevelSentry } from "@toruslabs/loglevel-sentry";
 import loglevel from "loglevel";
 
 const log = loglevel.getLogger("web3auth-logger");
-// TODO: remove this once we make Sentry optional in loglevel-sentry package
-const loglevelPlugin = new LoglevelSentry({
-  getActiveSpan: () => undefined,
-  addBreadcrumb: () => {},
-  captureException: () => "",
-  getClient: (): undefined => undefined,
-});
+const loglevelPlugin = new LoglevelSentry();
 loglevelPlugin.install(log);
 
 export { log };
