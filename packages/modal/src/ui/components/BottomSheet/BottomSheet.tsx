f19bd676bee092504de933cbeb2e0bc23bed4c04
Tai Nguyen TT <tai.nguyen@tor.us>
2025-04-09 08:38:44 +0700

    fix border radius of bottom sheet

--- packages/modal/src/ui/components/BottomSheet/BottomSheet.tsx
+++ packages/modal/src/ui/components/BottomSheet/BottomSheet.tsx
@@ -5,7 +5,7 @@ import { BottomSheetProps } from "./BottomSheet.type";
  * @returns BottomSheet component
  */
 function BottomSheet({ isShown, onClose, children, uiConfig }: BottomSheetProps) {
-  const { borderRadiusType } = uiConfig;
+  const { borderRadiusType = "large" } = uiConfig;
   return (
     <>
       {/* Backdrop */}
@@ -23,7 +23,7 @@ function BottomSheet({ isShown, onClose, children, uiConfig }: BottomSheetProps)
       <div
         className={cn(
           `w3a--fixed w3a--bottom-2 w3a--left-2 w3a--mx-auto w3a--flex w3a--w-[96%] w3a--flex-col 
-      w3a--gap-y-2 w3a--rounded-3xl w3a--border w3a--border-app-gray-100 w3a--bg-app-white w3a--p-4 w3a--shadow-lg w3a--transition-transform w3a--duration-500 
+      w3a--gap-y-2 w3a--border w3a--border-app-gray-100 w3a--bg-app-white w3a--p-4 w3a--shadow-lg w3a--transition-transform w3a--duration-500 
       w3a--ease-out dark:w3a--border-app-gray-600 dark:w3a--bg-app-dark-surface-main
       ${isShown ? "w3a--translate-y-0 w3a--delay-700" : "w3a--translate-y-full"}`,
           {
