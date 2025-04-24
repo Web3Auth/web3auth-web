bad8c481463a94b09a3a87864380fa5e25ce62d5
Chaitanya Potti <chaitanya.potti@gmail.com>
2025-04-24 11:04:06 +0800

    remove ethereum mpc provider from default export

--- packages/no-modal/src/providers/index.ts
+++ packages/no-modal/src/providers/index.ts
@@ -1,5 +1,4 @@
 export * from "./account-abstraction-provider";
 export * from "./base-provider";
-export * from "./ethereum-mpc-provider";
 export * from "./ethereum-provider";
 export * from "./solana-provider";
