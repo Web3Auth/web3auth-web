a204bd19946d899b522df3d3ddd616a6734e31e1
Archit <arch1995@gmail.com>
2025-05-12 18:14:04 +0530

    updates footer

--- packages/modal/src/ui/components/Footer/Footer.tsx
+++ packages/modal/src/ui/components/Footer/Footer.tsx
@@ -32,13 +32,13 @@ function Footer({ privacyPolicy, termsOfService }: FooterProps) {
       <div className="w3a--flex w3a--items-center w3a--justify-center w3a--gap-2">
         <img
           height="36"
-          src="https://images.web3auth.io/metamask-footer-logo-light.svg"
+          src="https://images.web3auth.io/web3auth-footer-logo-light.svg"
           alt="Web3Auth Logo Light"
           className="w3a--block w3a--h-9 dark:w3a--hidden"
         />
         <img
           height="36"
-          src="https://images.web3auth.io/metamask-footer-logo-dark.svg"
+          src="https://images.web3auth.io/web3auth-footer-logo-dark.svg"
           alt="Web3Auth Logo Dark"
           className="w3a--hidden w3a--h-9 dark:w3a--block"
         />
