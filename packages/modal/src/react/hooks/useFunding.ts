48db63b3aecf582470794f165ec977eba10d128a
BboyStatix <statix231@hotmail.com>
2025-10-14 11:52:32 +0800

    add addFunding addReceive hooks and composables to modal pkg

---
+++ packages/modal/src/react/hooks/useFunding.ts
@@ -0,0 +1,37 @@
+import { BaseEmbedControllerState } from "@toruslabs/base-controllers";
+import { WalletServicesPluginError, Web3AuthError } from "@web3auth/no-modal";
+import { useCallback, useState } from "react";
+
+import { useWalletServicesPlugin } from "./useWalletServicesPlugin";
+
+export interface IUseFunding {
+  loading: boolean;
+  error: Web3AuthError | null;
+  showFunding: (showFundingParams?: BaseEmbedControllerState["showFunding"]) => Promise<void>;
+}
+
+export const useFunding = (): IUseFunding => {
+  const { plugin, ready } = useWalletServicesPlugin();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Web3AuthError | null>(null);
+
+  const showFunding = useCallback(
+    async (showFundingParams?: BaseEmbedControllerState["showFunding"]) => {
+      setLoading(true);
+      setError(null);
+      try {
+        if (!plugin) throw WalletServicesPluginError.notInitialized();
+        if (!ready) throw WalletServicesPluginError.walletPluginNotConnected();
+
+        await plugin.showFunding(showFundingParams);
+      } catch (error) {
+        setError(error as Web3AuthError);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [plugin, ready]
+  );
+
+  return { loading, error, showFunding };
+};
