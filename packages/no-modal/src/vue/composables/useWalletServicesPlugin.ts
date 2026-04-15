9469e5acb266447b2f952fcba6c20e978bb87bf5
lwin <lwin.kyaw@consensys.net>
2026-04-15 14:23:19 +0800

    feat: vue dedupe

--- packages/no-modal/src/vue/composables/useWalletServicesPlugin.ts
+++ packages/no-modal/src/vue/composables/useWalletServicesPlugin.ts
@@ -1,10 +1,11 @@
 import { inject } from "vue";
 
+import { WalletInitializationError } from "../../base";
 import { WalletServicesContextKey } from "../context/WalletServicesContext";
 import { IWalletServicesInnerContext } from "../interfaces";
 
 export const useWalletServicesPlugin = (): IWalletServicesInnerContext => {
   const context = inject<IWalletServicesInnerContext>(WalletServicesContextKey);
-  if (!context) throw new Error("WalletServicesContext not found");
+  if (!context) throw WalletInitializationError.fromCode(1000, "usage of useWalletServicesPlugin not wrapped in `WalletServicesContextProvider`.");
   return context;
 };
